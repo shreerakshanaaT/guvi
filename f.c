cd8c4fbc26242e3bc1d6c127d52033a8478abecb
shreerakshanaaT <rakshana.vasan@gmail.com>
2016-06-07 13:51:59 +0530

    Create f.c

---
+++ f.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int i,f=1,num;
+  printf("Enter a number");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+      f=f*i;
+  printf("Factorial of is: %d",f);
+  return 0;
+}
