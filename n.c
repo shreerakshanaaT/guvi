77ae601bd68e1b89616a7b6715ecd280544227f1
shreerakshanaaT <rakshana.vasan@gmail.com>
2016-06-07 14:15:34 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int r,n,rem;
+int main()
+{
+	// your code goes here
+	scanf("%d",&n);
+	while(n!= 0)
+	{
+        int rem = n%10;
+        r= r*10 + rem;
+        n=n/10;
+    }
+    printf("%d",r);
+	return 0;
+}
