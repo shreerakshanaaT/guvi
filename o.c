316d53c25dec544ebd8eb4428f7449a8ff472d61
shreerakshanaaT <rakshana.vasan@gmail.com>
2016-07-30 00:40:06 +0530

    Update o.c

--- o.c
+++ o.c
@@ -3,6 +3,8 @@ int main()
 {
 	int n,c=0,i;
 scanf("%d",&n);
+if(n>0)
+{
 for(i=1;i<=n;i++)
 {
 	if(i%2!=0)
@@ -19,5 +21,10 @@ else
 	c=c+2;
 }
 printf("%d",c);
+}
+else
+{
+	printf("0");
+}
 return 0;
 }
